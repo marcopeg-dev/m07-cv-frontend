243ecf63419fa2200bbf6aa6ebb6fa7b323e928d
marcopeg-dev <marco.pegoraro+dev@gmail.com>
2020-02-18 16:47:51 +0000

    Customize home screen

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>Hello M07</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
