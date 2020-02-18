e90136440809d4d08580fa0d74352ebad3a6a44c
Marco Pegoraro <marco.pegoraro@gmail.com>
2020-02-18 19:31:03 +0000

    Add react-router and a basic route for the public view of the profile #1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import ProfileView from "./ProfileView";
 import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello M07</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Hello M07</h1>
+        <h2>Start editing to see some magic happen!</h2>
+      </div>
+      <Route path="/:uname" component={ProfileView} />
+    </Router>
   );
 }
