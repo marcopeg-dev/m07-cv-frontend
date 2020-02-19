fd82e64cc6758d4cd1f826926c2cb18227460225
Marco Pegoraro <marco.pegoraro@gmail.com>
2020-02-19 12:38:04 +0000

    Basic hook to fetch data from backend #4

--- src/views/ProfileView.jsx
+++ src/views/ProfileView.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useBackend } from "../state/use-backend";
 
 const ProfileView = ({ match }) => {
+  const { data, error, isLoading } = useBackend(match.params.uname);
+
+  const body = (() => {
+    if (isLoading) return <div>loading...</div>;
+
+    if (error) return <div>{error.message}</div>;
+
+    return (
+      <div>
+        name: {data.name}
+        <br />
+        surname: {data.surname}
+      </div>
+    );
+  })();
+
   return (
     <div>
       <h3>profile {match.params.uname}</h3>
@@ -13,6 +30,8 @@ const ProfileView = ({ match }) => {
           <Link to={`/`}>go home</Link>
         </li>
       </ul>
+      <hr />
+      <div>{body}</div>
     </div>
   );
 };
