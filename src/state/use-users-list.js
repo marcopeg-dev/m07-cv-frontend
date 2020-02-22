9783089eda4900d56879f229d3ff4a9770074236
VernitaJ <vernitajones87@gmail.com>
2020-02-22 16:21:22 +0000

    Added state component to show DB & changed UI to show ID only fixes#12

---
+++ src/state/use-users-list.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+export const UsersList = uname => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://m07.herokuapp.com/`)
+      .then(response => response.json())
+      .then(users => setUsers(users))
+      .catch(err => console.error(err));
+  }, []);
+
+  return {
+    users
+  };
+};
--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -1,17 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
+import { UsersList } from "../state/use-users-list";
 
-const HomeView = ({ match, uname }) => {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    fetch(`https://m07.herokuapp.com/`)
-      .then(response => response.json())
-      .then(users => setUsers(users))
-      .catch(err => console.error(err));
-  }, []);
-
-  console.log("hey", users);
+const HomeView = ({ uname }) => {
+  const { users } = UsersList(uname);
 
   return (
     <div>
@@ -19,20 +11,21 @@ const HomeView = ({ match, uname }) => {
         {users.length > 0 ? (
           users.map(user => (
             <div key={user.id}>
-            <Link to={`/${user.id}`}>
-            <li>
-            <h2 className="homeview-user-names">{user.data.name} {user.data.surname}</h2>
-            </li>
-            </Link>
+              <Link to={`/${user.id}`}>
+                <li>
+                  <h2 className="homeview-user-names">{user.id} </h2>
+                </li>
+              </Link>
             </div>
           ))
         ) : (
           <div>
             <h2 className="site-heading">There are no users to display</h2>
           </div>
-          )}
-    </ul>
+        )}
+      </ul>
     </div>
-)};
+  );
+};
 
 export default HomeView;
