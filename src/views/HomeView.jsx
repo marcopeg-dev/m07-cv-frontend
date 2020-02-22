0d34652a516c640630eee23428be6e64ee4a7469
VernitaJ <vernitajones87@gmail.com>
2020-02-22 12:25:45 +0000

    Added data list to homeview  component and added slight styling #12

--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-const HomeView = ({ match }) => {
+const HomeView = ({ match, uname }) => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://m07.herokuapp.com/`)
+      .then(response => response.json())
+      .then(users => setUsers(users))
+      .catch(err => console.error(err));
+  }, []);
+
+  console.log("hey", users);
+
   return (
     <div>
       <ul>
-        <li>
-          <Link to="/marcopeg">Open Marcopeg</Link>
-        </li>
-        <li>
-          <Link to="/luke-skywalker">Open Luke Skywalker</Link>
-        </li>
-      </ul>
+        {users.length > 0 ? (
+          users.map(user => (
+            <div key={user.id}>
+            <Link to={`/${user.id}`}>
+            <li>
+            <h2 className="homeview-user-names">{user.data.name} {user.data.surname}</h2>
+            </li>
+            </Link>
+            </div>
+          ))
+        ) : (
+          <div>
+            <h2 className="site-heading">There are no users to display</h2>
+          </div>
+          )}
+    </ul>
     </div>
-  );
-};
+)};
 
 export default HomeView;
