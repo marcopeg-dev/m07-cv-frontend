237c4d147ca37497ffb2b9c4f50da5ff24c09915
VernitaJ <vernitajones87@gmail.com>
2020-02-22 09:11:09 +0000

    Updated code to show edit option only for existing users.
    Fixes marcopeg-dev/m07-cv-frontend#9

--- src/layouts/ProfileViewLayout/ProfileViewLayout.jsx
+++ src/layouts/ProfileViewLayout/ProfileViewLayout.jsx
@@ -1,20 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useBackend } from "../../state/use-backend";
 
-const ProfileViewLayout = ({ uname, children }) => (
-  <div>
-    <h3>profile {uname}</h3>
-    <ul>
-      <li>
-        <Link to={`/${uname}/edit`}>Edit profile</Link>
-      </li>
-      <li>
-        <Link to={`/`}>go home</Link>
-      </li>
-    </ul>
-    <hr />
-    <div>{children}</div>
-  </div>
-);
+const ProfileViewLayout = ({ uname, children }) => {
+  // const uname = match.params;
+  const { data, error, isLoading } = useBackend(uname);
+  console.log("this", data);
+
+  const editOption = () => {
+    if (!error) {
+      return (
+        <li>
+          <Link to={`/${uname}/edit`}>Edit profile</Link>
+        </li>
+      );
+    }
+  };
+
+  return (
+    <div>
+      <h3>profile {uname}</h3>
+      <ul>
+        {editOption()}
+        <li>
+          <Link to={`/`}>go home</Link>
+        </li>
+      </ul>
+      <hr />
+      <div>{children}</div>
+    </div>
+  );
+};
 
 export default ProfileViewLayout;
