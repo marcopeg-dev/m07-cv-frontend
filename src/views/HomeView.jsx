d3383098dd148fb0fae9bb2f63506466f5a4d597
Maya Mounzer <maeydelee14@yahoo.com>
2020-02-23 23:07:55 +0000

    Creates user styles in home view users
    #19

--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { UsersList } from "../state/use-users-list";
+import "../components/HomeViewUsers/users.css";
 
 const HomeView = ({ uname }) => {
   const { users } = UsersList(uname);
@@ -10,12 +11,18 @@ const HomeView = ({ uname }) => {
       <ul>
         {users.length > 0 ? (
           users.map(user => (
-            <div key={user.id}>
+            <div key={user.id} className="home-view-users">
               <Link to={`/${user.id}`}>
                 <li>
-                  <h2 className="homeview-user-names">
-                    {user.name} {user.surname}
+                  <img
+                    src={user.profile_pic}
+                    alt={`${user.name}'s profile pic`}
+                  />
+                  <h2>
+                    {user.name} <br />
+                    {user.surname}
                   </h2>
+                  <div className="button" />
                 </li>
               </Link>
             </div>
