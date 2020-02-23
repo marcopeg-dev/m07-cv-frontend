0b153bf1f5f9bad90f6e8ab103e878238e5bd6d8
marcopeg-dev <marco.pegoraro+dev@gmail.com>
2020-02-23 12:23:33 +0000

    Remove backend from layout
    fixes #9

--- src/layouts/ProfileViewLayout/ProfileViewLayout.jsx
+++ src/layouts/ProfileViewLayout/ProfileViewLayout.jsx
@@ -1,12 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useBackend } from "../../state/use-backend";
 
-const ProfileViewLayout = ({ uname, children }) => {
-  // const uname = match.params;
-  const { data, error, isLoading } = useBackend(uname);
-  console.log("this", data);
-
+const ProfileViewLayout = ({ uname, error, children }) => {
   const editOption = () => {
     if (!error) {
       return (
