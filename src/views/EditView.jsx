28364985f532a431782f64de1fe6268f9f11518b
Ömer <omerfsozcu@gmail.com>
2020-02-23 19:25:49 +0000

    Fixes marcopeg-dev/m07-cv-frontend
    #10

--- src/views/EditView.jsx
+++ src/views/EditView.jsx
@@ -1,11 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { UsersList } from '../state/use-users-list'
 
-const EditView = ({ match }) => {
+const EditView = ({ match, children, uname }) => {
+
+  const { users } = UsersList(uname);
+  const initalFormState = {}
+  const [value, setValue] = useState('hi')
+
+  const onChange = event => {
+    const newValue = event.target.value;
+    setValue([...value, newValue])
+    console.log(value)
+  }
+
+  const onSubmit = event => {
+    event.preventDefault()
+    console.log(value)
+  }
+  console.log(users)
+
+
   return (
     <div>
-      <h3>edit {match.params.uname}</h3>
-      <Link to={`/${match.params.uname}`}>View profile</Link>
+      <div> <h3>edit {match.params.uname}</h3>
+        <form onSubmit={onSubmit}>
+          <label>Name</label>
+          <input value={match.params.name} name='name' type="text" onChange={onChange}></input>
+          {/* {users.map(function (user, i) {
+            console.log(user.id)
+            return (
+              user.id === match.params.uname ?
+                <input value={user.name} name='name' type="text" onChange={onChange} /> :
+                <input value={''}
+                />
+            )
+          })
+          } */}
+          <label>Surname</label>
+          <input value={match.params.surname} name='name' type="text" onChange={onChange}></input>
+
+          <button>Save</button>
+        </form>
+        <Link to={`/${match.params.uname}`}>View profile</Link></div>
     </div>
   );
 };
