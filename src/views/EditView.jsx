b99fdee405f9b366af55134a61f8415c449f1de8
Nicola Winterson <nicola.l.winterson@gmail.com>
2020-03-04 10:39:23 +0000

    Closed string

--- src/views/EditView.jsx
+++ src/views/EditView.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { FetchUsers } from "../state/fetch-users
+import { FetchUsers } from "../state/fetch-users";
 import "../styles.css";
 
 const EditView = ({ match, children, uname }) => {
