ab3e34fb1be00d032a094c3eba092ff942f65934
Nicola Winterson <nicola.l.winterson@gmail.com>
2020-03-04 09:36:44 +0000

    Minor CSS update to EditView component

--- src/views/EditView.jsx
+++ src/views/EditView.jsx
@@ -1,42 +1,55 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { UsersList } from '../state/use-users-list'
+import { UsersList } from "../state/use-users-list";
+import "../styles.css";
 
 const EditView = ({ match, children, uname }) => {
-
   const { users } = UsersList(uname);
-  const initalFormState = {}
-  const [value, setValue] = useState('hi')
+  const initalFormState = {};
+  const [value, setValue] = useState("hi");
 
   const onChange = event => {
     const newValue = event.target.value;
-    setValue([...value, newValue])
-    console.log(value)
-  }
+    setValue([...value, newValue]);
+    console.log(value);
+  };
 
   const onSubmit = event => {
-    event.preventDefault()
-    console.log(value)
-  }
-  console.log(users)
-
+    event.preventDefault();
+    console.log(value);
+  };
+  console.log(users);
 
   return (
     <div>
-      <div>
-        <h3>edit {match.params.uname}</h3>
-        <form onSubmit={onSubmit} >
-          <label>Name
-            <input name='name' type="text" onChange={onChange} placeholder={match.params.name}></input>
+      <div className="editview">
+        <h3 className="editview__header">edit {match.params.uname}</h3>
+        <form className="editview__form" onSubmit={onSubmit}>
+          <label className="editview__label">
+            Name
+            <input
+              className="editview__input"
+              name="name"
+              type="text"
+              onChange={onChange}
+              placeholder={match.params.name}
+            />
           </label>
-          <label>Surname
-            <input name='name' type="text" onChange={onChange} placeholder={match.params.surname}></input>
+          <label className="editview__label">
+            Surname
+            <input
+              className="editview__input"
+              name="name"
+              type="text"
+              onChange={onChange}
+              placeholder={match.params.surname}
+            />
           </label>
-          < button >Save</button>
+          <button className="editview__button">Save</button>
         </form>
-        <Link to={`/${match.params.uname}`}>View profile</Link></div>
-    </div >
+        <Link to={`/${match.params.uname}`}>View profile</Link>
+      </div>
+    </div>
   );
 };
-
 export default EditView;
